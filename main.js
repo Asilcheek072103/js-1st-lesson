39df72f9b0a3ba88d381dbf0590f2e13b92dc5ad
unknown <123@mail.com>
2023-01-09 14:29:58 +0500

    homework

---
+++ main.js
@@ -0,0 +1,15 @@
+var str = prompt('Ismingizni kiriting')
+console.log('Sizning ismingiz: ' + str);
+alert('Xush kelibsiz: ' + str)
+
+var old = prompt('Yoshingizdi kiriting')
+console.log('Sizning yoshingiz= ' + old);
+alert('Sizning yoshingiz= ' + old)
+
+var nu1 = prompt('Birinchi misolni yeching: 15+7=?')
+console.log('1 chi misol: 15+7=' + (15+7)+ 'Sizning javobingiz esa '+ nu1);
+alert('Sizning javobingzing= '+ nu1 + ' ,Togri javob esa= ' +  (15+7))
+
+var nu2 = prompt ('2 chi misol: 6*7-8=?')
+console.log('2 chi misolning javobi: '+(6*7-8)+'Sizning javobingiz esa' + nu2 );
+alert('2 chi misolning javobi: '+(6*7-8)+'. Sizning javobingiz esa ' + nu2 )
